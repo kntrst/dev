c1c96253908a209afc3584cbf0ae29d39787e282
kntrst <ga.olli@gmail.com>
2024-05-29 20:26:46 +0200

    groundwork

--- concept.rs
+++ concept.rs
@@ -9,3 +9,57 @@ pub struct SymbolId{
 pub struct ModuleName{
     name: String
 }
+
+// deserializing:
+// 1) from_str => Aws => Command
+// 2) from_bytes => Awb => Command
+
+// serializing
+// 1) as_str => Aws => String
+// 2) as_vec => Awb => Vec<u8>
+
+struct Command{
+    instance_id: InstanceId,
+    payload: Payload
+}
+struct InstanceId{
+    id: u16
+}
+enum Payload{
+    AudioPump(AudioPump),
+    AudioStop(AudioStop),
+    SetValue(SetValue),
+    GetValue(GetValue),
+    SetValues(SetValues),
+    GetValues(GetValues)
+}
+struct AudioPump{}
+struct AudioStop{}
+struct SetValue{
+    module: Module,
+    variable: Variable,
+    value: Value
+}
+struct Module{
+    id: u32,
+    name: String
+}
+struct Variable{
+    mask: u32,
+    offset: u32,
+    name: String
+}
+enum Value{
+    Uint(u32),
+    Int(i32),
+    Float(f32)
+}
+impl Command{
+    fn as_str(&mut self) -> Result<String>{
+        let aws = Aws::from(self)
+        aws.deserialize()
+    }
+}
+impl 
+#[cfg(test)]
+mod tests;
---
+++ tests.rs
@@ -0,0 +1,5 @@
+#[test]
+fn test{
+    let string = "0,co,m.v[o],1";
+    let command = de::from_str(&mut string);
+}
